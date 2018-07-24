4ed1fda95dd17abe35efa73a23708cc3636c77f6
NGUYENDUC\NGUYEN DUC <duc.nguyen@dtt.vn>
2018-07-24 15:59:12 +0700

    Create typescript react and call api

--- ReactJsCRUD/ClientApp/components/NavMenu.tsx
+++ ReactJsCRUD/ClientApp/components/NavMenu.tsx
@@ -4,7 +4,7 @@ import { Link, NavLink } from 'react-router-dom';
 export class NavMenu extends React.Component<{}, {}> {
     public render() {
         return <div className='main-nav'>
-                <div className='navbar navbar-inverse'>
+            <div className='navbar navbar-inverse' style={{ backgroundColor: '#3f4c3de3', borderColor: '#080808'}}>
                 <div className='navbar-header'>
                     <button type='button' className='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>
                         <span className='sr-only'>Toggle navigation</span>
@@ -12,24 +12,19 @@ export class NavMenu extends React.Component<{}, {}> {
                         <span className='icon-bar'></span>
                         <span className='icon-bar'></span>
                     </button>
-                    <Link className='navbar-brand' to={ '/' }>ReactJsCRUD</Link>
+                    <Link className='navbar-brand' to={'/'}>Employee</Link>
                 </div>
                 <div className='clearfix'></div>
                 <div className='navbar-collapse collapse'>
                     <ul className='nav navbar-nav'>
                         <li>
-                            <NavLink to={ '/' } exact activeClassName='active'>
+                            <NavLink to={'/'} exact activeClassName='active'>
                                 <span className='glyphicon glyphicon-home'></span> Home
                             </NavLink>
                         </li>
                         <li>
-                            <NavLink to={ '/counter' } activeClassName='active'>
-                                <span className='glyphicon glyphicon-education'></span> Counter
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={ '/fetchdata' } activeClassName='active'>
-                                <span className='glyphicon glyphicon-th-list'></span> Fetch data
+                            <NavLink to={'/fetchemployee'} activeClassName='active'>
+                                <span className='glyphicon glyphicon-th-list'></span> Fetch employee
                             </NavLink>
                         </li>
                     </ul>
---
+++ ReactJsCRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ReactJsCRUD.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ReactJsCRUD.Controllers
+{
+    [Route("api/Employee")]
+    public class EmployeeController : Controller
+    {
+        EmployeeDataAccess objemployee = new EmployeeDataAccess();
+        // GET: api/<controller>
+        [HttpGet]
+        [Route("Index")]
+        public IEnumerable<TblEmployee> Index()
+        {
+            return objemployee.GetAllEmployee();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet]
+        [Route("Details/{id}")]
+        public TblEmployee Details(int id)
+        {
+            return objemployee.GetEmployeeData(id);
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        [Route("Create")]
+        public int Create(TblEmployee employee)
+        {
+            return objemployee.AddEmployee(employee);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut]
+        [Route("Edit")]
+        public int Edit(TblEmployee employee)
+        {
+            return objemployee.UpdateEmployee(employee);
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete]
+        [Route("Delete/{id}")]
+        public int Delete(int id)
+        {
+            return objemployee.DeleteEmployee(id);
+        }
+        // GET city
+        [HttpGet]
+        [Route("GetCityList")]
+        public IEnumerable<TblCities> Details()
+        {
+            return objemployee.GetCities();
+        }
+    }
+}
